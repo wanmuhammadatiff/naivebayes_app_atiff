f8e4e7f4d2fc48b6fbbfdb6fa6a75852a56dcd9a
wanmuhammadatiff <wanmuhammadatiff@gmail.com>
2025-11-15 12:06:22 +0800

    import sklearn

--- nb_app.py
+++ nb_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn as sk
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
